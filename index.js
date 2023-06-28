e66a279f6fcac58a12dfd8ff6ef927e8660cac82
james lay <james@eventrentalsystems.com>
2023-06-28 13:59:49 -0600

    add demonstration flow

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ let main;
 app.get('/', function(req, res) {
     const id = Math.random().toString(36).substring(2, 15) +
         Math.random().toString(36).substring(2, 15);
-    res.send(`<a href='/${id}'>start</a>`);
+    // TODO - passthrough query params
+    res.redirect(`/${id}?token=12345`);
 });
 
 app.get("/:id/", (req, res) => {
@@ -23,24 +24,26 @@ app.get("/:id/", (req, res) => {
     if (conns[id]) {
         conns[id](value);
     } else {
-        main(id);
+        main(id, value);
     }
 });
 
-function show(id, html) {
-    return new Promise((resolve, reject) => {
+const show = id => html => 
+    new Promise((resolve, reject) => {
         conns[id] = resolve;
         evts.emit(id, html);
     });
-}
 
 
 module.exports = {
     show,
     start(port, entrypoint) {
-        main = async function(id) {
-            await entrypoint(id);
-            for (;;) await show(id, "done");
+        main = async function(id, args={}) {
+            const showfn = show(id);
+            await entrypoint({id, show:showfn, args});
+
+            // TODO - allow user to customize 'done' behavior:
+            for (;;) await showfn("done");
         };
         app.listen(port, () => console.log("Server started on port %d", port));
     },
--- test.js
+++ test.js
@@ -1,81 +1,77 @@
-const {show, start} = require("./index.js");
+const {start} = require("./index.js");
 const users = {
+    "newbie": {
+        name: "newbie",
+        password: "password",
+        token: "12345",
+    },
     "admin": {
         name: "admin adminson",
         password: "pass",
+        token: null,
     },
 };
 
-async function main(id) {
-    const user = await login(id);
-    loop: for (;;) {
-        const {choice} = await show(id, mainMenu(id, user));
-        switch (choice) {
-            case "logout":
-                await show(id, "Bye!");
-                return;
-            case "profile":
-                await show(id, profileMenu(id, user));
-                break;
-            case "settings":
-                await show(id, settingsMenu(id, user));
-                break;
-            case "back":
-                await show(id, `going back to main menu<a href='/${id}'>ok</a>`);
-                continue;
-            default:
-                await show(id, "Unknown choice");
-                break loop;
-        }
+async function main({show, id, args}) {
+
+    // 0. store the token and link it to the account:
+    const {token} = args;
+
+    const user = Object.values(users).find(u => u.token === token);
+    if (!user) {
+        await show("Invalid token");
+        return;
     }
+
+    await show(`Hello, ${user.name}. Let's get you set up. <a href='/${id}/'>next</a>`);
+
+    const {pmngr} = await show(`Do you use a password manager? <br />` +
+        `<a href='/${id}?pmngr=yes'>yes</a> <a href='/${id}?pmngr=no'>no</a>`);
+
+    if (pmngr === "no") {
+        // show the top 5 password managers, including the built in android, microsoft, and apple ones:
+        // also this list of links should open in a new window using target _blank.
+        await show("You will not be able to use this service without a password manager " +
+            "because it relies on an authentication protocol called TOTP."+
+            "TOTP requires a secret key that is stored in your password manager.<br />"+
+            "Let's get you set up with a new password manager:<br />"+
+            "We HIGHLY recommend 1password. It is the most secure and easiest to use.<br />"+
+            "Install one of the following password managers or choose your own:<br />"+
+            "<a href='https://www.1password.com/' target='_blank'>1Password</a><br />"+
+            "<a href='https://www.lastpass.com/' target='_blank'>LastPass</a><br />"+
+            "<a href='https://keepass.info/' target='_blank'>KeePass</a><br />"+
+            "<a href='https://bitwarden.com/' target='_blank'>Bitwarden</a><br />"+
+            "<a href='https://www.dashlane.com/' target='_blank'>Dashlane</a><br />"+
+            "<a href='https://www.roboform.com/' target='_blank'>RoboForm</a><br />"+
+            "<a href='https://www.truekey.com/' target='_blank'>True Key</a><br />" +
+            `<a href='/${id}/'>next (or skip)</a>`
+        );
+    }
+
+    // 1. help the user get set up with a password manager.
+    const {mname} = await show("What which password manager will you use?<br />" +
+        `<a href='/${id}?mname=1password'>1Password</a><br />` +
+        `<a href='/${id}?mname=lastpass'>LastPass</a><br />` +
+        `<a href='/${id}?mname=keepass'>KeePass</a><br />` +
+        `<a href='/${id}?mname=bitwarden'>Bitwarden</a><br />` +
+        `<a href='/${id}?mname=dashlane'>Dashlane</a><br />` +
+        `<a href='/${id}?mname=roboform'>RoboForm</a><br />` +
+        `<a href='/${id}?mname=truekey'>True Key</a><br />` +
+        `<a href='/${id}?mname=other'>Other</a><br />`
+    );
+
+    // 2. show them the qr code so they can get TOTP codes.
+    await show("Now that you have a password manager, you can use it to store your secret key.<br />");
+    const {qr} = await show("Scan this QR code with your password manager to store your secret key:<br />" +
+        `<img src='https://chart.googleapis.com/chart?chs=166x166&chld=L|0&cht=qr&chl=otpauth://totp/My%20App%3A${user.name}%3Fsecret%3D${user.password}%26issuer%3DMy%2520App' />`
+    );
+
+    // 3. show them the login page.
+    for (;;) {
+        await show("Now that you have your secret key stored in your password manager, you can use it to log in.<br />" +
+            "<a href='https://go.ourers.com/'>Log in</a>");
+    }
+
 }
 
-async function login(id) {
-    for (let i = 0; i < Infinity; i++) {
-        const {username, password} = await show(id, loginpage(id));
-        if (users[username]?.password === password) {
-            await show(id, `success! <a href='/${id}'>continue</a>`);
-            return users[username];
-        } else {
-            await show(id, `bah, try again! <a href='/${id}'>continue</a>`);
-        }
-    }
-}
-
-start(3000, main);
-
-const loginpage = id => `
-<form action="/${id}">
-    <input type="text" name="username" placeholder="username">
-    <input type="password" name="password" placeholder="password">
-    <input type="submit" value="login">
-</form>
-`;
-
-const mainMenu = (id, user) => `
-<h1>hello ${user.name}</h1>
-<a href="/${id}/?choice=logout">logout</a>
-<ul>
-    <li><a href="/${id}?choice=profile">users</a></li>
-    <li><a href="/${id}?choice=settings">settings</a></li>
-    <li><a href='/${id}?choice=back'>back</a></li>
-</ul>
-`;
-
-const profileMenu = (id, user) => `
-<h1>users</h1>
-<a href="/${id}">back</a>
-<ul>
-    <li><a href="/${id}/users/1">user 1</a></li>
-    <li><a href="/${id}/users/2">user 2</a></li>
-</ul>
-`;
-
-const settingsMenu = (id, user) => `
-<h1>settings</h1>
-<a href="/${id}">back</a>
-<ul>
-    <li><a href="/${id}/settings/1">setting 1</a></li>
-    <li><a href="/${id}/settings/2">setting 2</a></li>
-</ul>
-`;
+start(8080, main);
